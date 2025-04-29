bcca5a3e68f9a3de08827b1bbcc1ed00fdd5b061
alexk <alexk@gmail.com>
2025-04-29 13:13:33 +0300

    update

--- src/pages/ContactPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section class="flex flex-col justify-center items-center flex-1 px-8">
-    <h2 class="text-4xl font-bold mb-4">Contact page</h2>
-    <p class="text-lg text-gray-600">Давайте создадим что-то удивительное вместе.</p>
-  </section>
-</template>
-
-<script setup>
-</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,12 @@ import HomePage from "../pages/HomePage.vue";
 import FrontendPage from "../pages/FrontendPage.vue";
 import TelegramPage from "../pages/TelegramPage.vue";
 import AIPage from "../pages/AIPage.vue";
-import ContactPage from "../pages/ContactPage.vue";
 
 const routes = [
   { path: "/", component: HomePage },
   { path: "/frontend", component: FrontendPage },
   { path: "/telegram", component: TelegramPage },
-  { path: "/ai", component: AIPage },
-  { path: "/contact", component: ContactPage },
+  { path: "/ai", component: AIPage },  
 ];
 
 const router = createRouter({
