9c6df7f0073fd7a48615c689a94ffe590f4e8be0
Alexey Kagan <alexk@mrkter.io>
2025-10-23 02:38:02 +0300

    update

--- src/pages/FrontendPage.vue
+++ src/pages/FrontendPage.vue
@@ -93,7 +93,7 @@
       </div>
       
       <div class="experience__resume">
-        <a href="/files/alexey-kagan-cv.docx" download class="experience__resume-button">
+        <a href="/files/alexey-kagan-cv.pdf" download class="experience__resume-button">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
             <polyline points="7 10 12 15 17 10"></polyline>
@@ -165,8 +165,8 @@ const jobs = [
   },
   {
     position: 'Frontend Developer',
-    company: 'Webjet (Freelance Projects)',
-    period: 'Freelance',
+    company: 'Yandex',
+    period: '2016-2018',
     achievements: [
       'Developed over 10 responsive web applications',
       'Optimized UIs for mobile (UX +25%)',
